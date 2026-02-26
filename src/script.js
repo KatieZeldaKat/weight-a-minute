030f347369672c2c18c8b282b9b22330bc3403bf
Katherine Norton <katherine@queerkat.games>
2026-02-26 00:57:03 -0600

    Disable buttons on press and create timer

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const secondsBetweenSets = 60;
+
+const timer = document.querySelector("#timer");
 const completedLabels = document.querySelectorAll("span");
 const buttons = document.querySelectorAll("button");
 
@@ -5,8 +8,27 @@ buttons.forEach((button) => {
 	button.addEventListener("click", () => {
 		const index = button.id.slice(-1);
 		incrementLabel(index);
+
+		setButtonsEnabled(false);
+
+		for (let second = secondsBetweenSets; second >= 0; second--) {
+			const delay = (secondsBetweenSets - second) * 1000;
+			setTimeout(() => {
+				timer.textContent = second;
+			}, delay);
+		}
+
+		setTimeout(() => {
+			setButtonsEnabled(true);
+		}, secondsBetweenSets * 1000);
 	});
 });
+
+function setButtonsEnabled(enabled) {
+	buttons.forEach((button) => {
+		button.disabled = !enabled;
+	});
+}
 
 function incrementLabel(index) {
 	const count = +completedLabels[index].textContent;
